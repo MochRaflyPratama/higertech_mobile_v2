f782c30faf9425ef993e7be81b02f3b8bbe92523
MochRaflyPratama <mochraflypratama05@gmail.com>
2025-06-18 08:01:42 +0700

    fix folder

--- lib/app/modules/folder/folder_repository.dart
+++ lib/app/modules/folder/folder_repository.dart
@@ -7,32 +7,33 @@ import 'package:shared_preferences/shared_preferences.dart';
 class FolderRepository {
   final String baseUrl = 'http://10.0.2.2:5101/api/MapPoints';
 
-  Future<List<FolderItemModel>> fetchMapPoints() async {
-    final prefs = await SharedPreferences.getInstance();
-    final token = prefs.getString('accessToken'); 
-    print('Token: $token');
+Future<List<FolderItemModel>> fetchMapPoints() async {
+  final prefs = await SharedPreferences.getInstance();
+  final token = prefs.getString('accessToken'); 
+  print('Token: $token');
 
-    if (token == null || token.isEmpty) {
-      throw Exception('Token tidak ditemukan. Silakan login kembali.');
-    }
-
-    final response = await http.get(
-      Uri.parse(baseUrl),
-      headers: {
-        'Authorization': 'Bearer $token',
-        'Content-Type': 'application/json',
-      },
-    );
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}'); 
-
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body);
-      return data.map((json) => FolderItemModel.fromJson(json)).toList();
-    } else {
-      throw Exception('Failed to load map points (${response.statusCode})');
-    }
+  if (token == null || token.isEmpty) {
+    throw Exception('Token tidak ditemukan. Silakan login kembali.');
   }
+
+  final response = await http.get(
+    Uri.parse(baseUrl),
+    headers: {
+      'Authorization': 'Bearer $token',
+      'Content-Type': 'application/json',
+    },
+  );
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}'); 
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> decoded = json.decode(response.body);
+    final List<dynamic> data = decoded['data'] ?? [];
+    return data.map((json) => FolderItemModel.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load map points (${response.statusCode})');
+  }
+}
 
   Future<void> deleteMapPoint(int id) async {
     final prefs = await SharedPreferences.getInstance();
